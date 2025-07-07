6a1689eee0947e4b612eeee34e996b80e6fec18e
kasi Durgarao <kasidurgarao183@gmail.com>
2025-07-07 23:03:00 +0530

    optionalselect file random function changed

--- src/components/OptionSelector.jsx
+++ src/components/OptionSelector.jsx
@@ -1,58 +1,57 @@
-import React, { useState } from 'react'
-let data = [["India","Hyderabad","Chennai"],["Plv","Tpg","Kvr","Rjy"],["Mango","Banana","Apple"]]
+import React, { useState } from "react";
+let data = [
+  ["India", "Hyderabad", "Chennai"],
+  ["Plv", "Tpg", "Kvr", "Rjy"],
+  ["Mango", "Banana", "Apple"],
+];
 const OptionSelector = () => {
-    const [text,setText] = useState(data[0][0]);
-    const [count,setCount] = useState(0)
-    const [selectOption,setSelectOption] = useState('')
+  const [text, setText] = useState(data[0][0]);
+  const [count, setCount] = useState(0);
 
-
-const getrandom=()=>{
-    let index = Math.floor(Math.random()*data.length);
-    let subIndex = Math.floor(Math.random()*data[index].length)
-    setText(data[index][subIndex]) 
-    setSelectOption("")
-}
-
-    const selecthandler=(e)=>{
-        let name = e.target.value;
-        console.log(name)
-        if(text === name){
-            setCount((prev)=>prev+1)
-            setSelectOption(name)
-            getrandom()
-        }
+  const getrandom = () => {
+    let newText = text;
+    while (newText === text) {
+      let index = Math.floor(Math.random() * data.length);
+      let subIndex = Math.floor(Math.random() * data[index].length);
+      newText = data[index][subIndex];
     }
+    setText(newText);
+  };
 
-
+  const selecthandler = (e) => {
+    let name = e.target.value;
+    console.log(name);
+    if (text === name) {
+      setCount((prev) => prev + 1);
+      setSelectOption(name);
+      getrandom();
+    }
+  };
 
   return (
     <div>
-
-        <div className='flex gap-4 justify-center items-center h-screen flex-col w-full '>
-            <div className='border w-40'>
-               <span>{text}</span>
-            </div>
-            <div className='flex gap-4'>
-            {
-                data.map((ele,idx)=>
-                <div  key={idx}>
-                    <select className='w-28 border' value={selectOption} onChange={selecthandler}>
-                        <option value="select">select</option>
-                        {
-                            ele.map((val,index)=>
-                            <option value={val} key={index} >{val}</option>
-                            )
-                        }
-                    </select>
-                </div>
-                
-                )
-            }
-            </div>
-            {count}
+      <div className="flex gap-4 justify-center items-center h-screen flex-col w-full ">
+        <div className="border w-40">
+          <span>{text}</span>
         </div>
+        <div className="flex gap-4">
+          {data.map((ele, idx) => (
+            <div key={idx}>
+              <select className="w-28 border" onChange={selecthandler}>
+                <option value="select">select</option>
+                {ele.map((val, index) => (
+                  <option value={val} key={index}>
+                    {val}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ))}
+        </div>
+        {count}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OptionSelector
+export default OptionSelector;
